ae95143a3ea30e4665e7f3434a0d8e61604e6ffd
Kishore1121 <Kishorekumar7914@gmail.com>
2023-05-23 17:32:22 +0530

    first

---
+++ src/books.js
@@ -0,0 +1,29 @@
+
+import img1 from './image/book1.jpg';
+import img2 from './image/Book2.jpg';
+import img3 from './image/book3.jpg'
+
+export const books =[
+    {
+        title:'Atomic Habits',
+        img:img1,
+        author:'James clear',
+        id:1,
+
+    },
+    {
+        title:'Who will cry when you die?',
+        img:img2,
+        author:'Robin sharma',
+        id:2,
+        
+      },
+      {
+        title:'Money,and the Law of Attraction',
+        img:img3,
+        author:'Esther',
+        id:3,
+        
+      },
+]
+
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+import './index.css';
+import {books} from'./books';
+import Book from "./Book";
+
+function Books() {
+    return (
+        <>
+            <h1 style={{textAlign:'center', marginTop:'30px'}}>Amazon best sellers</h1>
+            <section className="booklist">
+                {books.map((book)=>{
+                    // console.log(book);
+                return <Book {...book} key={book.id}/>
+                })}
+            </section>
+        </>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Books/>);
